7ed238717386e23c6b60247a742d70c83f258670
Alexandre <alexandre.fournou@gmail.com>
2020-05-03 23:00:54 +0200

    Swup transition

--- src/js/modules/home-image.js
+++ src/js/modules/home-image.js
@@ -1,10 +1,7 @@
 const projectLinks = document.querySelectorAll(".projects__item a");
 
-console.log(projectLinks);
-
 projectLinks.forEach((link) =>
     link.addEventListener("click", (e) => {
-        console.log(e);
         // e.preventDefault();
     })
 );
--- src/js/modules/pages-transition.js
+++ src/js/modules/pages-transition.js
@@ -1,29 +1,50 @@
-import barba from "@barba/core";
 import gsap from "gsap";
+import Swup from "swup";
+import SwupJsPlugin from "@swup/js-plugin";
 
-function delay(n) {
-    n = n || 2000;
-    return new Promise((done) => {
-        setTimeout(() => {
-            done();
-        }, n);
-    });
-}
+let options = [
+    {
+        from: "/",
+        to: "/",
+        in: function () {
+            pageTransitionOut();
+        },
+        out: (next) => {
+            pageTransitionIn();
+            gsap.to(document.querySelector("#swup"), {
+                onComplete: next,
+            });
+        },
+    },
+];
 
 let linkClicked = null;
 const projectLinks = document.querySelectorAll(".projects__item");
 
+console.log(projectLinks);
 projectLinks.forEach((link) => {
     link.addEventListener("click", () => {
-        linkClicked = link;
+        document.addEventListener("swup:clickLink", (event) => {
+            console.log("Coucou");
+            setTimeout(function () {
+                console.log("Coucoufin");
+            }, 5000);
+        });
     });
 });
 
+const swup = new Swup({
+    plugins: [new SwupJsPlugin(options)],
+});
+
 // Function to add and remove the page transition screen
-function pageTransition() {
+function pageTransitionIn() {
     let tl = gsap.timeline();
     tl.to(".screen__white", { duration: 0.5, transform: "translateY(-100%)" });
     tl.to(".screen__black", { duration: 0.5, transform: "translateY(-100%)", delay: "-0.35" });
+}
+function pageTransitionOut() {
+    let tl = gsap.timeline();
     tl.to(".screen__black", { duration: 0.5, transform: "translateY(-200%)", delay: 0.8 });
     tl.to(".screen__white", { duration: 0.5, transform: "translateY(-200%)", delay: "-0.35" });
     tl.set(".screen__black", { transform: "translateY(0)" });
@@ -31,8 +52,8 @@ function pageTransition() {
 }
 
 function pageTransition2() {
-    const projectImg = linkClicked.querySelector(".project__img");
-    console.log(projectImg);
+    // const projectImg = linkClicked.querySelector(".project__img");
+    // console.log(projectImg);
 
     let tl = gsap.timeline();
     // tl.set(projectImg, { duration: 0.5, clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)" });
@@ -44,15 +65,4 @@ function pageTransition2() {
     tl.set(".screen__white", { transform: "translateY(0)" });
 }
 
-barba.init({
-    transitions: [
-        {
-            name: "default-transition",
-            leave(data) {
-                pageTransition();
-                return gsap.to(data.current.container, {});
-            },
-            enter(data) {},
-        },
-    ],
-});
+function image() {}
